7fe486b14c3072daaf740e2f54283e4b7d2c123c
Emile14 <emilianoanaya111@gmail.com>
2024-04-22 21:13:06 -0600

    Reto 11  completo

---
+++ src/edu/emiliano_anaya/reto11/ui/Esp.java
@@ -0,0 +1,10 @@
+package edu.emiliano_anaya.reto11.ui;
+
+public class Esp extends Idiomas {
+    public Esp(){
+        BIENVENIDO = "Bienvenido a nuestra bibliolteca";
+        SALIDA = "Salir";
+        SELECCION = "Selecciona el libro para analizar: ";
+        ERROR = "Opcion no disponible";
+    }
+}
